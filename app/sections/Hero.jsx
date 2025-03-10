7080a1a7c19e7af713de7353e1c8c9b60d74e115
Azlan Baig <135128027+Azlan-Baig@users.noreply.github.com>
2025-03-10 11:53:05 +0500

    Updated

--- app/sections/Hero.jsx
+++ app/sections/Hero.jsx
@@ -38,12 +38,13 @@ const Hero = () => {
       </div>
         <Leva hidden/>
       <div className="w-full h-full absolute inset-0">
-        <Canvas className="w-full h-full mt-32">
+        <Canvas className="w-full h-full ">
           <Suspense fallback={<CanvasLoader />}>
-            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
             <HeroCamera isMobile={isMobile}>
             <HackerRoom scale={sizes.deskScale} position={sizes.deskPosition} rotation={[0.1,-Math.PI,0]} />
             </HeroCamera>
+            group
             <ambientLight intensity={1} />
             <directionalLight position={[10,10,10]} intensity={0.5} /> 
           </Suspense>

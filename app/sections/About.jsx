a3b9f3ab9738299015fc4d203e07865e76d1ffbb
Azlan Baig <135128027+Azlan-Baig@users.noreply.github.com>
2025-03-13 11:45:38 +0500

    About Section Added

---
+++ app/sections/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='c-space my-20'> 
+      <h1 className='grid xl:grid-cols-3 xl:grid-rows-6 md:grid-col-2 grid-cols-1 gap-5 h-full bg-red-600'>
+      </h1>
+    </section>
+  )
+}
+
+export default About

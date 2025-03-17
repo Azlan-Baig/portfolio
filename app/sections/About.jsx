ef06b6cae3951f2ec9b8c35e3af9500544ada069
Azlan Baig <135128027+Azlan-Baig@users.noreply.github.com>
2025-03-17 11:40:48 +0500

    About Section Completed

--- app/sections/About.jsx
+++ app/sections/About.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import Globe from "react-globe.gl";
+import Button from "../components/Button";
 
 const About = () => {
+  const [hasCopied, setHasCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText('azlanbaig.dev@gmail.com');
+    setHasCopied(true);
+
+    setTimeout(() => {
+      setHasCopied(false);
+    }, 2000);
+  };
   return (
     <section className="c-space my-20">
       <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-col-2 grid-cols-1 gap-5 h-full">
@@ -12,7 +24,7 @@ const About = () => {
               className="w-full sm:h-[276px] h-fit object-contain"
             />
             <div>
-              <p className="grid-headtext">Hi,I am Azlan</p>
+              <p className="grid-headtext">Hi, I am Azlan</p>
               <p className="grid-subtext">
                 With 9 years of experience, I have honed my skills in both
                 frontend and backend dev, creating dynamic and responsive
@@ -32,9 +44,70 @@ const About = () => {
             <div>
               <p className="grid-headtext">Tech Stack</p>
               <p className="grid-subtext">
-                I specialize in a variety of languages, frameworks, and tools
-                that allow me to build robust and scalable applications
+                I specialize in full-stack development with a focus on
+                JavaScript and TypeScript. I create dynamic, responsive
+                frontends using frameworks like React/Next, and build scalable
+                backends with Node.js and frameworks such as Express or NestJS.
+                Leveraging TypeScript, I ensure high code quality and
+                maintainability in every project.
               </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+              <Globe
+                height={300}
+                width={300}
+                backgroundColor="rgba(0, 0, 0, 0)"
+                backgroundImageOpacity={0.5}
+                showAtmosphere
+                showGraticules
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+              />
+            </div>
+            <div>
+              <p className="grid-headtext">
+                I’m very flexible with time zone communications & locations
+              </p>
+              <p className="grid-subtext">
+                I&apos;m based in Rjieka, Croatia and open to remote work
+                worldwide.
+              </p>
+              <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-2 xl:row-span-3">
+          <div className="grid-container">
+            <img src="assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
+
+            <div>
+              <p className="grid-headtext">My Passion for Coding</p>
+              <p className="grid-subtext">
+                I love solving problems and building things through code. Programming isn&apos;t just my
+                profession—it&apos;s my passion. I enjoy exploring new technologies, and enhancing my skills.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img
+              src="assets/grid4.png"
+              alt="grid-4"
+              className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+            />
+
+            <div className="space-y-2">
+              <p className="grid-subtext text-center">Contact me</p>
+              <div className="copy-container" onClick={handleCopy}>
+                <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
+                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">azlanbaig.dev@gmail.com</p>
+              </div>
             </div>
           </div>
         </div>

d01e568690a05b15c13e7167c2edc04daf3e807e
Azlan Baig <135128027+Azlan-Baig@users.noreply.github.com>
2025-04-03 00:39:32 +0500

    Footer Section Added.

--- app/page.jsx
+++ app/page.jsx
@@ -6,16 +6,17 @@ import About from "./sections/About";
 import Projects from "./sections/Projects";
 import Clients from "./sections/Clients";
 import Contact from "./sections/Contact";
-
+import Footer from "./sections/Footer";
 function Home() {
   return (
-    <main className="max-w-full mx-auto">
+    <main className="max-w-7xl mx-auto">
       <Navbar />
       <Hero/>
       <About/>
       <Projects/>
       <Clients/>
       <Contact/>
+      <Footer/>
     </main>
   );
 }
